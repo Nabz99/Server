9f2ad72e739f853c5b3bb1c6b1ec1f198145de3c
Nabz99 <nabil.taleb.dz@viacesi.fr>
2024-05-16 10:08:50 +0100

    3rd

--- src/classes/PubClass.js
+++ src/classes/PubClass.js
@@ -1,42 +1,42 @@
-import { create as _create, find as _find, findById as _findById, updateOne, deleteOne } from "../models/PubModel"
+const PubSchema = require("../models/PubModel")
 
 class Pub {
 
     static async create(pub) {
 
-        const t = await _create(pub)
+        const t = await PubSchema.create(pub)
         return t
     }
 
     static async find() {
 
-        const t = await _find().populate('client').sort({ $natural: -1 }).exec()
+        const t = await PubSchema.find().populate('client').sort({ $natural: -1 }).exec()
         return t
     }
 
     static async findById(pub_id) {
 
-        const t = await _findById(pub_id).exec()
+        const t = await PubSchema.findById(pub_id).exec()
         return t
     }
 
     static async update(id, pub) {
 
-        const t = await updateOne({ _id: id }, { $set: pub }).exec()
+        const t = await PubSchema.updateOne({ _id: id }, { $set: pub }).exec()
         return t
     }
 
     static async remove(pub) {
 
-        const t = await deleteOne({ _id: pub }).exec()
+        const t = await PubSchema.deleteOne({ _id: pub }).exec()
         return t
     }
 
     static async findByClient(clientId) {
 
-        const t = await _find({client: clientId}).sort({ $natural: -1 }).exec()
+        const t = await PubSchema.find({client: clientId}).sort({ $natural: -1 }).exec()
         return t
     }
 }
 
-export default Pub
+module.exports = Pub
--- src/routes/pub.js
+++ src/routes/pub.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const Pub = require("../classes/PubClass");
 const Client = require('../classes/ClientClass');
+const Pub = require('../classes/PubClass');
 const router = express.Router();
 
 
